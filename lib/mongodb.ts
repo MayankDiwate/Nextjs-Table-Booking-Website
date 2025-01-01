0efef84d644d95d52ef40e0c61e0ae596f5eec19
Mayank Diwate <mayankdiwate007@gmail.com>
2025-01-01 17:50:53 +0530

    minor fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,25 +4,12 @@ const uri = process.env.NEXT_PUBLIC_MONGO_URI as string;
 const options = {};
 
 if (!uri) {
-  throw new Error("NEXT_PUBLIC_MONGO_URI is not defined. Check your environment variables.");
+  throw new Error(
+    "NEXT_PUBLIC_MONGO_URI is not defined. Check your environment variables."
+  );
 }
 
-// declare global {
-//   let _mongoClientPromise: Promise<MongoClient> | undefined;
-// }
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+const client: MongoClient = new MongoClient(uri, options);
+const clientPromise: Promise<MongoClient> = client.connect();
 
 export default clientPromise;
